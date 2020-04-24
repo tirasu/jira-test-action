73c84f7f37eda9869f9a46711f5677af7eb0201e
Maciej Wikira <maciej.daniel.wikira@gmail.com>
2020-04-24 17:43:31 +0200

    Add optional mvn test arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 echo "Cleaning build"
 mvn clean
 echo "Run unit tests"
-mvn test
+if [[ $1 != 'none' ]]; then
+  mvn $1 test
+else
+  mvn test
+fi
 echo "Running unit & integration tests"
 mvn integration-test
