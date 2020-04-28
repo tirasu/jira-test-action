683e0005740aa297a7e8f8a188fefc6f733f1e7f
mdwikira <58301537+mdwikira@users.noreply.github.com>
2020-04-28 14:14:24 +0200

    Remove additional args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 echo "Cleaning build"
 mvn clean
 echo "Run unit tests"
-if [[ $1 != 'none' ]]; then
-  mvn $1 test
-else
-  mvn test
-fi
+mvn test
 echo "Running unit & integration tests"
 mvn integration-test
