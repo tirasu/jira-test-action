262b565ec86d2e39dfe905c34d866543e39c11f3
mdwikira <58301537+mdwikira@users.noreply.github.com>
2020-10-29 18:26:17 +0100

    fix: fail if integration tests fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,18 @@ mvn clean
 echo "Running unit & integration tests"
 if [[ $1 != 'none' ]]; then
   mvn integration-test $1
+  if [[ "$?" -ne 0 ]] ; then
+    echo 'could not perform tests'; exit $rc
+  fi  
 else
   mvn integration-test
+  if [[ "$?" -ne 0 ]] ; then
+    echo 'could not perform tests'; exit $rc
+  fi
 fi
-if [[ "$?" -ne 0 ]] ; then
-  echo 'could not perform tests'; exit $rc
-else
-  if [[ $2 != 'none' ]]; then
+
+if [[ $2 != 'none' ]]; then
   mvn package $2
-  else
-    mvn package
-  fi  
-fi
+else
+  mvn package
+fi  
