066eb6932abfcfde93d122a3e9f9b2c990022958
mdwikira <58301537+mdwikira@users.noreply.github.com>
2020-10-29 16:44:14 +0100

    feat: prevent integration test silent fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,18 @@
 
 echo "Cleaning build"
 mvn clean
-echo "Run unit tests"
-if [[ $1 != 'none' ]]; then
-  mvn test $1
-else
-  mvn test
-fi
 echo "Running unit & integration tests"
 if [[ $1 != 'none' ]]; then
   mvn integration-test $1
 else
   mvn integration-test
 fi
-if [[ $2 != 'none' ]]; then
-  mvn package $2
+if [[ "$?" -ne 0 ]] ; then
+  echo 'could not perform tests'; exit $rc
 else
-  mvn package
+  if [[ $2 != 'none' ]]; then
+  mvn package $2
+  else
+    mvn package
+  fi  
 fi
